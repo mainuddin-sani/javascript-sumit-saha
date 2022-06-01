d26b79bf9e3554318fc9197e87b3868028246c21
mainuddin-sani <main12sani@gmail.com>
2022-06-01 17:40:39 +0600

    global scope, local scope, block scope

--- javascript-practice.js
+++ javascript-practice.js
@@ -1,7 +1,7 @@
 // javascript function
 
 const userName = (name)=>{
-    console.log(`${name} is a most popular language`);
+    document.write(`${name} is a most popular language`);
 }
 userName('Javascript');
 setTimeout(()=>{
@@ -9,3 +9,43 @@ setTimeout(()=>{
 }, 3000)
 
 console.log('Hello!')
+
+
+let b = 'Javascript Programming';
+
+const showData = ()=>{
+    b = 3;
+}
+
+console.log('before', b);
+showData();
+console.log('after', b);
+
+
+// local scope
+
+let name = 'Mianuddin';
+
+const showName = ()=>{
+    let x = "Sani";
+    console.log(name +' '+ b);
+}
+showName();
+
+// console.log(x);
+
+// block scope 
+
+let first = 'Hello!';
+
+const daialog = ()=> {
+    let second  = "Mainuddin";
+    console.log(first+ ' ' + second);
+    if(second == 'Mainuddin'){
+        let third = "Sani";
+        console.log(first + " " + second + " " + third);
+    }
+    console.log(first + " " + second + " " + third);
+}
+
+daialog();
