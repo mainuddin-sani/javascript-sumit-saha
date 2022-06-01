583164ab4580a9c6eefc372aa412946ab84988e9
mainuddin-sani <main12sani@gmail.com>
2022-06-01 16:54:02 +0600

    fucntion practice

---
+++ javascript-practice.js
@@ -0,0 +1,11 @@
+// javascript function
+
+const userName = (name)=>{
+    console.log(`${name} is a most popular language`);
+}
+userName('Javascript');
+setTimeout(()=>{
+    userName('Python');
+}, 3000)
+
+console.log('Hello!')
